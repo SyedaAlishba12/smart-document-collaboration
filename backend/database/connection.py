3f16d689eea174118c51eddccd73869298b8dcef
SyedaAlishba12 <syedaalishbakhatoonl@gmail.com>
2026-08-27 21:21:42 +0500

    resolved error in connection.py

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -1,14 +1,9 @@
 import os
-from pathlib import Path
 
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine
 
-
-BASE_DIR = Path(__file__).resolve().parents[2]
-ENV_FILE = BASE_DIR / ".env"
-
-load_dotenv(ENV_FILE)
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
